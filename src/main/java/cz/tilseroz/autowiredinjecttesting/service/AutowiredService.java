54780c7b070bebd4ed017aa068e62b2e4607bd75
David Tilšer <davidtilser@gmail.com>
2021-10-02 22:46:54 +0200

    Deleted unused imports

--- src/main/java/cz/tilseroz/autowiredinjecttesting/service/AutowiredService.java
+++ src/main/java/cz/tilseroz/autowiredinjecttesting/service/AutowiredService.java
@@ -7,7 +7,6 @@ import cz.tilseroz.autowiredinjecttesting.service.toinject.StoreService;
 import cz.tilseroz.autowiredinjecttesting.service.toinject.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 @Service
--- src/test/java/cz/tilseroz/autowiredinjecttesting/service/ConstructorServiceTest.java
+++ src/test/java/cz/tilseroz/autowiredinjecttesting/service/ConstructorServiceTest.java
@@ -2,17 +2,11 @@ package cz.tilseroz.autowiredinjecttesting.service;
 
 import cz.tilseroz.autowiredinjecttesting.entity.Book;
 import cz.tilseroz.autowiredinjecttesting.service.toinject.BookService;
-import cz.tilseroz.autowiredinjecttesting.service.toinject.EmailService;
-import cz.tilseroz.autowiredinjecttesting.service.toinject.StoreService;
 import cz.tilseroz.autowiredinjecttesting.service.toinject.UserService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
